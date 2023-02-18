557b11781a7ed85383693fdc74ddd67c93114e75
a-imantha <imantha.ahangama@gmail.com>
2023-02-19 02:44:43 +0530

    Adding pre-processing

--- src/modules/xai.py
+++ src/modules/xai.py
@@ -1,13 +1,34 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+from processing.utils import process_input_seq
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+SAMPLE_SEQUENCES = {
+    "2018_08_12_08_00": "src/samples/Inp_Seq_2018_08_12_08_00.zip"
+}
+
 def create_explainable_ai_page():
 
     st.write("""
     # Explainable AI for Precipitation Nowcasting - Demo
 
     Dataset Used for this is the Meteonet Dataset by Meteo France: [Meteonet](https://meteofrance.github.io/meteonet)
+    
+    The inputs should be 11 images with size 128*128. Contents of the images are explained below with the file name format they should bear.
     """)
+
+    st.write("""
+        You can select a sample datetime stamp on which the data is recorded and click download. After downloading extract the downloaded zip folder and re-upload the list of images using the file uploader.
+        """)
+
+    selected_sequence = st.selectbox("Select a sequence", list(SAMPLE_SEQUENCES.keys()))
+    zip_file_path = SAMPLE_SEQUENCES[selected_sequence]
+    with open(zip_file_path, "rb") as f:
+        bytes_data = f.read()
+        st.download_button("Download Sequence", data=bytes_data, file_name=zip_file_path, mime="application/zip")
+
 
     expected_files = ['rr_0.png', 'rr_15.png', 'rr_30.png', 'rr_45.png', 'rr_60.png', 'wu_0.png', 'wu_60.png',
                       'wv_0.png', 'wv_60.png', 'sat_0.png', 'sat_60.png']
@@ -21,23 +42,61 @@ def create_explainable_ai_page():
                 st.warning(f"File {file} not found in uploaded data. Please check your files and try again.")
 
         for file in uploaded_files:
-            image = Image.open(file)
+            image = Image.open(file).convert('L')
+            image = np.array(image)
+            image = remove_zero_pad(image)
+            image = Image.fromarray(image)
             if image.size != (128, 128):
-                image = image.resize((128, 128))
-            image_array = np.array(image)
-            images[file.name] = image_array
+                image = image.resize((128, 128), resample=Image.LANCZOS)
+
+            images[file.name] = np.array(image)
 
     with st.expander("Input Sequence", expanded=True):
         if images:
-            col1, col2, col3, col4, col5, col6 = st.columns(6)
-            col1.image(images.get("rr_0.png"), width=128, caption="Rain Radar at t = 0")
-            col2.image(images.get("rr_15.png"), width=128, caption="Rain Radar at t = 15")
-            col3.image(images.get("rr_30.png"), width=128, caption="Rain Radar at t = 30")
-            col4.image(images.get("rr_45.png"), width=128, caption="Rain Radar at t = 45")
-            col5.image(images.get("rr_60.png"), width=128, caption="Rain Radar at t = 60")
-            col6.image(images.get("wu_0.png"), width=128, caption="Wind U Component at t = 0")
-            col1.image(images.get("wu_60.png"), width=128, caption="Wind U Component at t = 60")
-            col2.image(images.get("wv_0.png"), width=128, caption="Wind U Component at t = 0")
-            col3.image(images.get("wv_60.png"), width=128, caption="Wind U Component at t = 60")
-            col4.image(images.get("sat_0.png"), width=128, caption="Satellite at t = 0")
-            col5.image(images.get("sat_60.png"), width=128, caption="Satellite at t = 60")
+            plot_seq(images)
+
+    processed_input = 0
+    if images:
+        images, processed_input = process_input_seq(images)
+
+    with st.expander("Processed Sequence", expanded=True):
+        if processed_input == 1:
+            plot_seq(images)
+
+def fig2img(img):
+    """Convert a Matplotlib figure to a PIL Image and return it"""
+    import io
+    buf = io.BytesIO()
+    fig, ax = plt.subplots()
+    ax.set_axis_off()  # remove axis ticks and labels
+    fig.tight_layout(pad=0)
+    ax.imshow(img, cmap='viridis')
+
+    fig.savefig(buf)
+    buf.seek(0)
+    img = Image.open(buf)
+    return img
+def plot_seq(seq):
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+
+    col1.image(fig2img(seq.get("rr_0.png")), use_column_width=True, caption="Rain Radar at t = 0", )
+    col2.image(fig2img(seq.get("rr_15.png")), use_column_width=True, caption="Rain Radar at t = 15")
+    col3.image(fig2img(seq.get("rr_30.png")), use_column_width=True, caption="Rain Radar at t = 30")
+    col4.image(fig2img(seq.get("rr_45.png")), use_column_width=True, caption="Rain Radar at t = 45")
+    col5.image(fig2img(seq.get("rr_60.png")), use_column_width=True, caption="Rain Radar at t = 60")
+    col6.image(fig2img(seq.get("wu_0.png")), use_column_width=True, caption="Wind U Component at t = 0")
+    col1.image(fig2img(seq.get("wu_60.png")), use_column_width=True, caption="Wind U Component at t = 60")
+    col2.image(fig2img(seq.get("wv_0.png")), use_column_width=True, caption="Wind U Component at t = 0")
+    col3.image(fig2img(seq.get("wv_60.png")), use_column_width=True, caption="Wind U Component at t = 60")
+    col4.image(fig2img(seq.get("sat_0.png")), use_column_width=True, caption="Satellite at t = 0")
+    col5.image(fig2img(seq.get("sat_60.png")), use_column_width=True, caption="Satellite at t = 60")
+
+def remove_zero_pad(image):
+    dummy = np.argwhere(image < 245) # assume blackground is zero
+    max_y = dummy[:, 0].max()
+    min_y = dummy[:, 0].min()
+    min_x = dummy[:, 1].min()
+    max_x = dummy[:, 1].max()
+    crop_image = image[min_y:max_y, min_x:max_x]
+
+    return crop_image
--- src/processing/utils.py
+++ src/processing/utils.py
@@ -1,2 +1,57 @@
-#
-# def validate_input():
+import constants as const
+import numpy as np
+
+
+def __pre_process_rain_radar_image(image):
+    image = np.clip(image, const.RAIN_RADAR_LEFT_CUTOFF, const.RAIN_RADAR_RIGHT_CUTOFF)
+    image =  np.log(np.add(image, 1))/np.log(const.RAIN_RADAR_RIGHT_CUTOFF + 1)
+    return image
+
+def __pre_process_sat_image(image):
+
+    # Correct the pixel conversions
+    image = ((image*89.0)/255.0) - 69
+
+    image = np.clip(image, -69, 20)
+
+    image = -((image - const.SAT_MEAN) / const.SAT_STD)
+    image = (image + abs(const.SAT_MIN)) / (abs(const.SAT_MIN) + const.SAT_MAX)
+    return image
+
+def __pre_process_wind_u(image):
+
+    # Correct the pixel conversions
+    image = ((image*48.0)/255.0) - 16
+
+    image = np.clip(image, -16, 32)
+
+    image = ((image - const.WIND_U_MEAN) / const.WIND_U_STD)
+    image = (image + abs(const.WIND_U_MIN)) / (abs(const.WIND_U_MIN) + const.WIND_U_MAX)
+    return image
+
+def __pre_process_wind_v(image):
+
+    # Correct the pixel conversions
+    image = ((image*45.0)/255.0) - 22
+
+    image = np.clip(image, -22, 23)
+
+    image = ((image - const.WIND_V_MEAN) / const.WIND_V_STD)
+    image = (image + abs(const.WIND_V_MIN)) / (abs(const.WIND_V_MIN) + const.WIND_V_MAX)
+    return image
+def process_input_seq(images_dic):
+    images_dic['rr_0.png'] = __pre_process_rain_radar_image(images_dic['rr_0.png'])
+    images_dic['rr_15.png'] = __pre_process_rain_radar_image(images_dic['rr_15.png'])
+    images_dic['rr_30.png'] = __pre_process_rain_radar_image(images_dic['rr_30.png'])
+    images_dic['rr_45.png'] = __pre_process_rain_radar_image(images_dic['rr_45.png'])
+    images_dic['rr_60.png'] = __pre_process_rain_radar_image(images_dic['rr_60.png'])
+    images_dic['sat_0.png'] = __pre_process_sat_image(images_dic['sat_0.png'])
+    images_dic['sat_60.png'] = __pre_process_sat_image(images_dic['sat_60.png'])
+    images_dic['wu_0.png'] = __pre_process_wind_u(images_dic['wu_0.png'])
+    images_dic['wu_60.png'] = __pre_process_wind_u(images_dic['wu_60.png'])
+    images_dic['wv_0.png'] = __pre_process_wind_v(images_dic['wv_0.png'])
+    images_dic['wv_60.png'] = __pre_process_wind_v(images_dic['wv_60.png'])
+
+    return images_dic, 1
+
+# def __pre_process_wind_u_images():
